eae44af36602598ccd45eb3897f777443c237636
momosenda <momo.senda@rakus.co.jp>
2018-11-07 15:49:49 +0900

    日付の表示を修正（年月日表示へ）

--- src/main/java/jp/co/rakus/stockmanagement/web/MemberForm.java
+++ src/main/java/jp/co/rakus/stockmanagement/web/MemberForm.java
@@ -1,5 +1,10 @@
 package jp.co.rakus.stockmanagement.web;
 
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+
 /**
  * メンバー関連のリクエストパラメータが入るフォーム.
  * @author igamasayuki
@@ -7,10 +12,15 @@ package jp.co.rakus.stockmanagement.web;
  */
 public class MemberForm {
 	/** 名前 */
+	@NotBlank(message="名前は必ず入力してください")
 	private String name;
 	/** メールアドレス */
+	@NotBlank(message="メールアドレス必ず入力してください")
+	@Email(message="e-mailの値が不正です")
 	private String mailAddress;
 	/** パスワード */
+	@NotBlank(message="パスワードは必ず入力してください")
+	@Size(min=1,max=16,message="1文字以上16文字以下で入力してください")
 	private String password;
 	public String getName() {
 		return name;
